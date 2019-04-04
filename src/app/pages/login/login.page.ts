69b0d5bf88103ce48b2ec0f9491ea580e23f3813
Luis Bocaranda <luisbocaranda19@gmail.com>
2019-04-04 09:07:34 -0430

    login page funcionando

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LoginService } from '../../providers/login/login.service';
+
+import { InspectTxtService } from '../../service/inspectTxt/inspect-txt.service';
+import { RouterModule, Routes, Router } from '@angular/router';
+import {}
+
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +15,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+	user = {
+		password:"",
+		username:""
+	}
+
+  constructor(public loginService: LoginService, public inspectTxt: InspectTxtService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  login(){
+	 
+    if(this.inspectTxt.notNullValueLogin(this.user)){
+		return;
+	}
+    this.loginService.login(this.user)
+      .subscribe(data => {
+        console.log(data);
+        if(data.status >= 200 && data.status < 300){
+        this.router.navigateByUrl('/menu/tabs');
+	  	}
+        this.alert.presentAlert(data.message,"OK para continuar");
+        
+        },error => {
+        this.alert.presentAlert("Error de conexion","Intente mas tarde");
+          console.log(error);
+        });
+		
   }
 
 }
